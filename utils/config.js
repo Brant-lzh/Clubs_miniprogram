38c71e32d6342b0884a4955e8fffa90f0cab0718
Brant <53347762+Brant-lzh@users.noreply.github.com>
2020-08-09 22:14:34 +0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,5 @@
 class Config {
     constructor() {}
 }
-Config.restUrl = 'http://www.m.com/api/v1/';
-// Config.restUrl = 'http://47.100.91.118:3008/api/v1/';
-// Config.restUrl = 'http://cxnmtw.lvguanjia88.com/api/v1/';
+Config.restUrl = 'http://www.m.com/api/v1/';//填写自己的IP
 export {Config};
